23cc50aa8785bc5668c7a5b75fe378a5c3cc5b23
amir <amirsmvb@yahoo.com>
2021-10-14 23:47:59 +0330

    Product model generated on models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Product(models.Model):
+    STATUS_CHOICES = (
+        ('publish', 'Publish'),
+        ('draft', 'Draft'),
+    )
+    title = models.CharField(max_length=80)
+    slug = models.CharField(max_length=100)
+    price = models.FloatField()
+    description = models.TextField(max_length=500)
+    image = models.ImageField(null=True, blank=True)
+    status = models.CharField(default='publish', max_length=20)
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
