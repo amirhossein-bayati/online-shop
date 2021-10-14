d438ea6a1f93a5b7afb8c83524b8e47e80e5f8cf
amir <amirsmvb@yahoo.com>
2021-10-15 00:03:58 +0330

    Product model updated on models.py

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 
 class Product(models.Model):
@@ -11,6 +13,7 @@ class Product(models.Model):
     slug = models.CharField(max_length=100)
     price = models.FloatField()
     description = models.TextField(max_length=500)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     image = models.ImageField(null=True, blank=True)
     status = models.CharField(default='publish', max_length=20)
     publish = models.DateTimeField(default=timezone.now)
