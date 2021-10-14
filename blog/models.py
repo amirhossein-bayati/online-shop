979f50c2a8c816f5e5eb32764ceafa398b7e2012
amir <amirsmvb@yahoo.com>
2021-10-15 00:11:51 +0330

    status choices added to Product model

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
     description = models.TextField(max_length=500)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     image = models.ImageField(null=True, blank=True)
-    status = models.CharField(default='publish', max_length=20)
+    status = models.CharField(default='publish', max_length=20, choices=STATUS_CHOICES)
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
