a1020066eab8ba27cb37ebc158040412c8f2c096
amir <amirsmvb@yahoo.com>
2021-10-15 00:08:22 +0330

    ProductAdmin generated on admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-# from .models import Product
-#
-#
-# @admin.register(Product)
-# class ProductAdmin(admin.ModelAdmin):
-#     list_display = ('title')
+from .models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publish', 'status')
+    list_filter = ('author', 'status')
+    list_editable = ('status',)
+    search_fields = ('title', 'description')
+    ordering = ('status', 'publish')
+    raw_id_fields = ('author',)
+    prepopulated_fields = {"slug": ("title",)}
+
