9fc48351301ade98cb7f65a91753a4f22f018e3e
amir <amirsmvb@yahoo.com>
2021-10-15 00:49:58 +0330

    statis files and template added to project

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homePage(request):
-    return HttpResponse("Home Page")
+    return render(request, 'blog/parents/base.html')
 
--- onlineShop/settings.py
+++ onlineShop/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# location where you will store your static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'blog/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
