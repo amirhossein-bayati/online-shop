48540ac2238b95728f102da94c67b5e1a3a2fac7
amir <amirsmvb@yahoo.com>
2021-10-15 00:57:35 +0330

    homepage(content) template added

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homePage(request):
-    return render(request, 'blog/parents/base.html')
+    return render(request, 'blog/partials/content.html')
 
