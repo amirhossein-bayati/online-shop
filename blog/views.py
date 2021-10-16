d922890ba07c6ad37fb8c13d04959f8f5193d326
amir <amirsmvb@yahoo.com>
2021-10-16 15:06:25 +0330

    paginating completed

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,32 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Product
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
 
 def homePage(request):
     products = Product.objects.filter(status='published')
 
+    # Paginate Site With 6 Items Per A Page
+    paginator = Paginator(products, 8)
+    page = request.GET.get('page')
+
+    # Post Of Certain Page
+    try:
+        products = paginator.page(page)
+
+    # If Page Not Integer
+    except PageNotAnInteger:
+        products = paginator.page(1)
+
+    # If Page NOt Exists
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
     context = {
         'products': products,
+        'page': page,
     }
     return render(request, 'blog/partials/content.html', context)
 
