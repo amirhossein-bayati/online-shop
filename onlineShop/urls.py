3545831b01534272ee84fcac33f8cc78b6ab439e
amir <amirsmvb@yahoo.com>
2021-10-28 17:27:54 +0330

    star rating added tp product details

--- onlineShop/urls.py
+++ onlineShop/urls.py
@@ -18,11 +18,12 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls', namespace='blog')),
+    re_path(r'^ratings/', include('star_ratings.urls', namespace='ratings')),
 ]
 
 if settings.DEBUG:
